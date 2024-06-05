b395a50c95cac2a6f169edb64bc2663051722434
806gw <shjea4364@naver.com>
2024-06-05 15:22:26 +0900

    feat: 타 웹사이트 코드를 가져와 안 내용 교체, 문제 및 문제 답안 추가 예정

---
+++ js/questions.js
@@ -0,0 +1,88 @@
+let questions = [
+  {
+    question: "문단을 강조하고 싶을 때 쓰는 태그는 ?",
+    answer: "strong 태그",
+    options: ["p 태그", "strong 태그", "div 태그", "span 태그"],
+  },
+  {
+    question: "제목을 쓰고 싶을 때, 사용되는 태그는 ?",
+    answer: "h1 ~ h6 태그",
+    options: ["h1 ~ h6 태그", "b 태그", "i 태그", "ins 태그"],
+  },
+  {
+    question: "구간을 정의할 때 사용하는 태그는 ?",
+    answer: "section 태그",
+    options: ["section 태그", "div 태그", "article 태그", "aside 태그"],
+  },
+  {
+    question: "하이퍼링크를 만들 때 사용하는 태그는 ?",
+    answer: "a 태그",
+    options: ["a 태그", "link 태그", "href 태그", "nav 태그"],
+  },
+  {
+    question: "이미지를 삽입할 때 사용하는 태그는 ?",
+    answer: "img 태그",
+    options: ["img 태그", "src 태그", "image 태그", "picture 태그"],
+  },
+  {
+    question: "테이블을 만들 때 사용하는 태그는 ?",
+    answer: "table 태그",
+    options: ["table 태그", "tr 태그", "td 태그", "th 태그"],
+  },
+  {
+    question: "목록을 만들 때 사용하는 태그는 ?",
+    answer: "ul 또는 ol 태그",
+    options: ["ul 또는 ol 태그", "list 태그", "li 태그", "menu 태그"],
+  },
+  {
+    question: "입력 필드를 만들 때 사용하는 태그는 ?",
+    answer: "input 태그",
+    options: ["input 태그", "form 태그", "field 태그", "textbox 태그"],
+  },
+  {
+    question: "폼을 제출할 때 사용하는 태그는 ?",
+    answer: "form 태그",
+    options: ["form 태그", "submit 태그", "button 태그", "action 태그"],
+  },
+  {
+    question: "스크립트를 삽입할 때 사용하는 태그는 ?",
+    answer: "script 태그",
+    options: ["script 태그", "js 태그", "code 태그", "style 태그"],
+  },
+  {
+    question: "기울임꼴로 표시하는 태그는 ?",
+    answer: "i 태그",
+    options: ["i 태그", "em 태그", "italic 태그", "b 태그"],
+  },
+  {
+    question: "선택 항목을 그룹화할 때 사용하는 태그는 ?",
+    answer: "optgroup 태그",
+    options: ["optgroup 태그", "option 태그", "select 태그", "list 태그"],
+  },
+  {
+    question:
+      "대화형 컨텐츠나 애플리케이션의 컨테이너를 정의할 때 사용하는 태그는 ?",
+    answer: "canvas 태그",
+    options: ["canvas 태그", "svg 태그", "div 태그", "section 태그"],
+  },
+  {
+    question: "줄 바꿈을 할 때 사용하는 태그는 ?",
+    answer: "br 태그",
+    options: ["br 태그", "hr 태그", "break 태그", "newline 태그"],
+  },
+  {
+    question: "페이지 내 특정 위치로 이동할 때 사용하는 태그는 ?",
+    answer: "a 태그",
+    options: ["a 태그", "link 태그", "nav 태그", "goto 태그"],
+  },
+  {
+    question: "대체 텍스트를 제공하여 이미지를 설명할 때 사용하는 속성은 ?",
+    answer: "alt 속성",
+    options: ["alt 속성", "title 속성", "desc 속성", "caption 속성"],
+  },
+  {
+    question: "메타데이터를 정의할 때 사용하는 태그는 ?",
+    answer: "meta 태그",
+    options: ["meta 태그", "link 태그", "style 태그", "script 태그"],
+  },
+];
